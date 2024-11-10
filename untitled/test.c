b069f20e5df9fd980ace46349a6bb68a0d5376b9
TariQ <241870159@smai.nju.edu.cn>
2024-11-10 08:58:13 +0800

    function B

--- untitled/test.c
+++ untitled/test.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int arr[2001];int copy[2001];
 int main(){
-	int n,t=0,min=10,flag=0;
+	int n,t=0,min=2000,flag=0;
 	scanf("%d",&n);
 	for(int i=0;i<n;i++) {
 		scanf("%d",&arr[i]);
@@ -17,12 +17,13 @@ int main(){
 			arr[flag]=arr[i-1];
 			arr[i-1]=t;
 			copy[flag]=arr[flag];
+
 			for(int j=i,l=0;j<n;j++,l++) {arr[j]=copy[n-1-l];}
 			break;
 		}
 	}
 	for(int i=0;i<n;i++) {
-		if(i=n-1){printf("%d",arr[i]);break;}
+		if(i==n-1){printf("%d",arr[i]);break;}
 		printf("%d ",arr[i]);
 	}
 	return 0;
