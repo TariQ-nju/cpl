89e7b3fb07e5c62c96c3b1b8580afcc6094d0318
TariQ <241870159@smai.nju.edu.cn>
2024-11-22 15:50:40 +0800

    cpl-9

--- untitled/stack.c
+++ untitled/stack.c
@@ -1,3 +1,4 @@
+8
 
 #include<stdio.h>
 #include<string.h>
--- untitled/test.c
+++ untitled/test.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
-
 int main(void){
-    
+    int t;
+    scanf("%d",&t);
+    while(t--) {
+        char str[10000];
+        scanf("%s",str);
+        
+
+    }
     return 0;
 }
