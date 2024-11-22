bf98b757866739a11aa9562be15ec6b76ea0c1d9
TariQ <241870159@smai.nju.edu.cn>
2024-11-22 22:57:39 +0800

    stack

--- untitled/stack.c
+++ untitled/stack.c
@@ -1,51 +1,32 @@
-8
-
 #include<stdio.h>
-#include<string.h>
-char stack[1000];
-void push(char*a,char b);
+#include<stdbool.h>
+#include <string.h>
 void pop(char*a);
-void top(char*a);
-void print(char*a);
-int main(){
-    int n;
-    char motion[1000]="\0";
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        scanf("%s",motion);
-        if(strcmp(motion,"push")==0) {
-            char c;
-            getchar();
-            scanf("%c",&c);
-            push(stack,c);
-        }
-        if(strcmp(motion,"pop")==0) pop(stack);
-        if(strcmp(motion,"top")==0) top(stack);
-        if(strcmp(motion,"print")==0) print(stack);
-    }
+char top(char*a);
+void push(char*a,char b);
+void reset(int *a);
+bool check(char a);
+char stack[100000];
+int le=0;
+int main(void) {
     return 0;
 }
-void push(char*a,char b){
-    int i=0;
-    while(a[i]!='\0') i++;
-    while(i>0) {a[i]=a[i-1];i--;}
-    a[0]=b;
+void reset(int *a) {
+    *a=0;
+}
+char top(char*a){
+    return a[le-1];
 }
 void pop(char*a){
-    if(*a=='\0'){printf("Empty\n");return;}
-    int len=0;
-    while(a[len]!='\0') len++;
-    for(int i=0;i<len-1;i++)a[i]=a[i+1];
-    a[len-1]='\0';
+    a[le-1]='\0';
+    le--;
 }
-void top(char*a){
-    if(*a=='\0'){printf("Empty\n");return;}
-    printf("%c\n",*a);
+void push(char*a,char b){
+    a[le]=b;
+    a[le+1]='\0';
+    le++;
 }
-void print(char*a){
-    if(*a=='\0'){printf("Empty\n");return;}
-    int len=0;
-    while(a[len]!='\0') len++;
-    for(int i=0;i<len;i++) printf("| %c |\n",a[i]);
-    printf("|===|");
+bool check(char a) {
+    if (a=='{'||a=='['||a=='(') return true;
+    else return false;
 }
